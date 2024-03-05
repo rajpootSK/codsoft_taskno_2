5fe0c333f9ce0048e3f6b21287fdc6be78b3ba73
rajpootSK <144593419+rajpootSK@users.noreply.github.com>
2024-03-05 14:56:19 +0530

    Add files via upload

---
+++ Student_Grade_Calculator.java
@@ -0,0 +1,39 @@
+   package MyPackage;
+import java.util.*;
+
+public class Student_Grade_Calculator {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Student Grade Calculator");
+		System.out.println("Enter no of Subjects");
+		int nS = sc.nextInt();
+		int total = 0;
+		for(int i=0; i<nS; i++) {
+			System.out.println("Enter marks obtain in "+(i+1)+":");
+			int marks = sc.nextInt();
+		    total += marks;	
+		}
+		double averP =(double)total/nS;
+        char Grade;
+        if(averP>=90) {
+        	Grade = 'O';
+        }else if(averP>=80) {
+        	Grade = 'A';
+        }else if(averP>=70) {
+        	Grade = 'B';
+        }else if(averP>=60) {
+        	Grade = 'C';
+        }else if(averP>=50) {
+        	Grade = 'D';
+        }else if(averP>=40) {
+        	Grade = 'E';
+        }else {
+        	Grade = 'F';
+        }
+        System.out.println("Total Marks Obtain is : "+total);
+        System.out.println("Average Percentage Gained is :"+averP+"%");
+        System.out.println("Grade :"+Grade);
+	}
+
+}
